aecbfa1fd362b7e7dde592a6c892ba73167dc71c
Aaron Molina <28906210+aaronm-git@users.noreply.github.com>
2021-11-13 15:58:55 -0500

    handleLogOut logic created

--- src/components/Settings.jsx
+++ src/components/Settings.jsx
@@ -1,4 +1,10 @@
-import { Container, Card } from "react-bootstrap";
+import { Container, Card, Button } from "react-bootstrap";
+import { BsFillDoorClosedFill } from "react-icons/bs";
+
+const handleLogOut = () => {
+  window.localStorage.clear();
+  window.location.href = "/authorize";
+};
 
 const Settings = () => {
   return (
@@ -6,6 +12,11 @@ const Settings = () => {
       <Card className="bg-dark text-white">
         <Card.Body>
           <Card.Title className="fw-light border-bottom border-primary">Settings</Card.Title>
+          <div className="text-end">
+            <Button variant="outline-danger" onClick={handleLogOut}>
+              Log Out <BsFillDoorClosedFill className="icon" />
+            </Button>
+          </div>
         </Card.Body>
       </Card>
     </Container>
