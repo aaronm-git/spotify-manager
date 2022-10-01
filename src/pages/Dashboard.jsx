4d7759a1c5c3761e5c8313e10550f26db7c1426a
Aaron Molina <28906210+aaronm-git@users.noreply.github.com>
2022-10-01 11:54:38 -0400

    file renaming

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -15,7 +15,7 @@ const Dashboard = () => {
 			<Card className="bg-dark text-white mt-2 shadow-lg">
 				<Card.Body>
 					<Card.Title className="fw-light border-bottom border-primary">Dashboard</Card.Title>
-					<TrackLibrary />
+					<SpotifyUserLibraryTable />
 				</Card.Body>
 			</Card>
 		);
