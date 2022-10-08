272ae5ce0d5cc24aa1dc81ecad129222117e51d3
Aaron Molina <28906210+aaronm-git@users.noreply.github.com>
2022-10-08 18:20:05 -0400

    update

--- src/components/Home/AuthorizeApp.jsx
+++ src/components/Home/AuthorizeApp.jsx
@@ -7,6 +7,7 @@ import { useAlert } from '../../hooks/alert';
 
 const handleGetAuth = () => {
 	const redirectUri = process.env.REACT_APP_SPOTIFY_REDIRECT_URI;
+	console.log(redirectUri);
 	const clientId = process.env.REACT_APP_SPOTIFY_CLIENT_ID;
 	const scopes = process.env.REACT_APP_SPOTIFY_SCOPES;
 	const url = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=code&redirect_uri=${redirectUri}&scope=${scopes}`;
