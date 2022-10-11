db9a4aadd18aa2ef25ed3ae72df3af9fa624b510
Aaron Molina <28906210+aaronm-git@users.noreply.github.com>
2022-10-10 23:22:48 -0400

    added footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,17 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-
 // Pages
 import Home from './pages/Home';
 import Dashboard from './pages/Dashboard';
 import Settings from './pages/Settings';
 import Callback from './pages/Callback';
 import NotFoundPage from './pages/404';
-
 // Components
 import PrivateRoute from './components/app/PrivateRoute';
 import AppAlert from './components/layouts/AppAlert';
 import Header from './components/layouts/Header';
-
+import Footer from './components/layouts/Footer';
 // Contexts
 import AlertContextProvider from './context/alerts/AlertState';
 import GlobalStateProvider from './context/GlobalState';
@@ -39,6 +37,7 @@ const App = () => {
 								<Route path="*" component={NotFoundPage} />
 							</Switch>
 						</Container>
+						<Footer />
 					</Router>
 				</AlertContextProvider>
 			</GlobalStateProvider>
