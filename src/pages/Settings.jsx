6ab0474cb2b138233dc5ce6bbc3c80c6c1377c28
Aaron Molina <28906210+aaronm-git@users.noreply.github.com>
2022-10-14 18:43:06 -0400

    logout functionality working

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,27 +1,16 @@
 import React from 'react';
-// hooks
-import { useLocation } from 'react-router-dom';
-import { useQueryClient } from '@tanstack/react-query';
 // components
 import { Container, Card, Button } from 'react-bootstrap';
-import { BsFillDoorClosedFill } from 'react-icons/bs';
+import LogoutButton from '../components/settings/LogoutButton';
 
 const Settings = () => {
-	const location = useLocation();
-	const queryClient = useQueryClient();
-	const handleLogOut = () => {
-		queryClient.clear();
-		location.pathname = '/';
-	};
 	return (
 		<Container className="mt-2">
 			<Card className="bg-dark text-white">
 				<Card.Body>
 					<Card.Title className="fw-light border-bottom border-primary">Settings</Card.Title>
 					<div className="text-end">
-						<Button variant="outline-danger" onClick={handleLogOut}>
-							Log Out <BsFillDoorClosedFill className="icon" />
-						</Button>
+						<LogoutButton />
 					</div>
 				</Card.Body>
 			</Card>
