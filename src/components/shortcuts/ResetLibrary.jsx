eb776c7452dca62e1e671fb39e8bb90c8c53de55
Aaron Molina <28906210+aaronm-git@users.noreply.github.com>
2022-10-13 22:35:57 -0400

    reset button style update

--- src/components/shortcuts/ResetLibrary.jsx
+++ src/components/shortcuts/ResetLibrary.jsx
@@ -3,6 +3,9 @@ import { Button } from 'react-bootstrap';
 import { useQueryClient } from '@tanstack/react-query';
 // contexts
 import TableDataContext from '../../context/dashboard/TableDataContext';
+// components
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { solid, regular, brands } from '@fortawesome/fontawesome-svg-core/import.macro';
 
 export default function ResetLibrary() {
 	const [, setData] = React.useContext(TableDataContext);
@@ -13,7 +16,8 @@ export default function ResetLibrary() {
 	}
 	return (
 		<Button size="lg" onClick={handleOnclick} className="mx-2">
-			Reset
+			<FontAwesomeIcon icon={solid('undo-alt')} />
+			<span className="mx-2">Reset</span>
 		</Button>
 	);
 }
