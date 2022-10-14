fec6fda2774ce4f39fc7ad2ea8697e24ad96a43f
Aaron Molina <28906210+aaronm-git@users.noreply.github.com>
2022-10-13 23:57:52 -0400

    clean up and updated logout to use reactquery

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,26 +1,32 @@
-import { Container, Card, Button } from "react-bootstrap";
-import { BsFillDoorClosedFill } from "react-icons/bs";
-
-const handleLogOut = () => {
-  window.localStorage.clear();
-  window.location.href = "/authorize";
-};
+import React from 'react';
+// hooks
+import { useLocation } from 'react-router-dom';
+import { useQueryClient } from '@tanstack/react-query';
+// components
+import { Container, Card, Button } from 'react-bootstrap';
+import { BsFillDoorClosedFill } from 'react-icons/bs';
 
 const Settings = () => {
-  return (
-    <Container className="mt-2">
-      <Card className="bg-dark text-white">
-        <Card.Body>
-          <Card.Title className="fw-light border-bottom border-primary">Settings</Card.Title>
-          <div className="text-end">
-            <Button variant="outline-danger" onClick={handleLogOut}>
-              Log Out <BsFillDoorClosedFill className="icon" />
-            </Button>
-          </div>
-        </Card.Body>
-      </Card>
-    </Container>
-  );
+	const location = useLocation();
+	const queryClient = useQueryClient();
+	const handleLogOut = () => {
+		queryClient.clear();
+		location.pathname = '/';
+	};
+	return (
+		<Container className="mt-2">
+			<Card className="bg-dark text-white">
+				<Card.Body>
+					<Card.Title className="fw-light border-bottom border-primary">Settings</Card.Title>
+					<div className="text-end">
+						<Button variant="outline-danger" onClick={handleLogOut}>
+							Log Out <BsFillDoorClosedFill className="icon" />
+						</Button>
+					</div>
+				</Card.Body>
+			</Card>
+		</Container>
+	);
 };
 
 export default Settings;
